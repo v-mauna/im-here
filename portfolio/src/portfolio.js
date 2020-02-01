e821fce58aa95f8d657569061035cff7afbddff7
Vanessa Mauna <vmauna@aol.com>
2020-02-01 14:28:31 -0500

    fix: Portfolio List Updated

--- portfolio/src/portfolio.js
+++ portfolio/src/portfolio.js
@@ -1,47 +1,71 @@
-import React from 'react'
-import './styles/portfolio.css'
-import selfie from './images/IMG_2589.jpeg'
+import React from 'react';
+import './styles/portfolio.css';
+import stypSC from './images/stypSC-macbook.png';
+import wrSC from './images/wrSC-iPhone.png';
 
 const Portfolio = () => {
-    return (
-        <div>
-            <div id="portfolio">
-                <div className="portfolio">
-                </div>
-                <div className='projectTitles' id='who-am-i'>
-                    <div className='projectsList'>
-                    <a href="http://s-t-y-p.herokuapp.com"> Shop Till You Pop</a>
-                    <p>
-                    An e-commerce site designed to give users access to a variety of cereals and introduce them to some new ones along the way.
-                    Shop Till You Pop utlizes the technologies listed below to allow users or guests to create carts, continue shopping, edit their carts, 
-                    and checkout. Yachts Don't Stop also takes care of inventory management, ensuring that any price changes are represented in a 
-                    user's cart until the time of purchase, and that no out-of-stock products are listed as available.
-
-                    <p>TECH STACK</p>
-                    <p>
-                        <ul>
-                    <li>React</li>
-                    <li>Redux</li>
-                    <li>Node.js</li>
-                    <li>Express</li>
-                    <li>PostgreSQL</li>
-                    <li>OAuth</li>
-                    </ul>
-                    </p>
-                    
-                    </p>
-                </div>
-                </div>
-                </div>
-        <div className="bottom">
-        <ul>
-        <div>
-            </div>
-            </ul>
-            </div>
-            </div>
-    );
+	return (
+		<div>
+			<div id="portfolio">
+				<div className="portfolio" />
+				<div className="projectTitles">
+					<div className="projectsList">
+						<br />
+						<img className="picture" src={stypSC} />
+						<div className="projectOne">
+							<a href="http://s-t-y-p.herokuapp.com"> Shop Till You Pop</a>
+							<p>
+								An e-commerce site designed to give users access to a variety of cereals and introduce
+								them to some new ones along the way. Shop Till You Pop utlizes the technologies listed
+								below to allow users or guests to create carts, continue shopping, edit their carts, and
+								checkout. Yachts Don't Stop also takes care of inventory management, ensuring that any
+								price changes are represented in a user's cart until the time of purchase, and that no
+								out-of-stock products are listed as available.
+								<p>TECH STACK</p>
+								<p>
+									<ul>
+										<li>React</li>
+										<li>Redux</li>
+										<li>Node.js</li>
+										<li>Express</li>
+										<li>PostgreSQL</li>
+										<li>OAuth</li>
+									</ul>
+								</p>
+							</p>
+						</div>
+						<br />
+						<img className="picture2" src={wrSC} />
+						<div className="projectTwo">
+							<a href="https://github.com/v-mauna/workoutsRemixed"> Workouts Remixed</a>
+							<p>
+								Created and designed as part of a 3-day Hackathon Project, Workouts Remixed was made for
+								those who desire to have more artistic liberties over their workouts. The app gives
+								users access to pre-designed workout programs while also allowing them to design and
+								store their own programs by accessing individual,targeted exercises as well as having
+								the option to choose exercises or circuits from the pre-designed programs.
+								<p>TECH STACK</p>
+								<p>
+									<ul>
+										<li>React Native</li>
+										<li>ReactNavigation</li>
+										<li>Node.js</li>
+										<li>Express</li>
+										<li>Javascript</li>
+									</ul>
+								</p>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="bottom">
+				<ul>
+					<div />
+				</ul>
+			</div>
+		</div>
+	);
 };
 
-export default Portfolio
-
+export default Portfolio;
---
+++ portfolio/src/workoutsRemixed.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './styles/portfolio.css'
+
+const WR = () => (
+<div className='projectsList'>
+                    <a href="http://s-t-y-p.herokuapp.com"> Workouts Remixed</a>
+                    <p>
+                    An e-commerce site designed to give users access to a variety of cereals and introduce them to some new ones along the way.
+                    Shop Till You Pop utlizes the technologies listed below to allow users or guests to create carts, continue shopping, edit their carts, 
+                    and checkout. Yachts Don't Stop also takes care of inventory management, ensuring that any price changes are represented in a 
+                    user's cart until the time of purchase, and that no out-of-stock products are listed as available.
+
+                    <p>TECH STACK</p>
+                    <p>
+                        <ul>
+                    <li>React</li>
+                    <li>Redux</li>
+                    <li>Node.js</li>
+                    <li>Express</li>
+                    <li>PostgreSQL</li>
+                    <li>OAuth</li>
+                    </ul>
+                    </p>
+                    
+                    </p>
+                </div>
+)
+
+export default WR
