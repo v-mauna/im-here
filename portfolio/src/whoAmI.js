ec48432c36dc46abbeec5fbf75b8e243481ba50b
Vanessa Mauna <vmauna@aol.com>
2020-02-04 13:18:39 -0500

    modifed and updated bio

--- portfolio/src/whoAmI.js
+++ portfolio/src/whoAmI.js
@@ -10,18 +10,31 @@ const WhoAmI = () => {
         <p />
         <img src={myPhoto} alt="Personal" />
         <p>
-          I'm Vanessa, a New York born and based full stack software engineer
-          with a background in restaurant management. My time in restaurants
-          enforced my passion for well thought out systems, crafting amazing experiences
-          and teams, collaborating, and the satisfaction that comes when form,
-          function and aesthetics meet and merge.
+          Hi! I'm Vanessa, a New York born and based full stack software
+          engineer with a background in restaurant management.
+          <p />
+          Working in the restaurant industry for over a decade, I enjoyed the
+          daily challenges that came along with managing teams of adults and
+          crafting exceptional guest experiences. I learned that I loved the
+          dynamic and collaborative nature of the work: filling in gaps and
+          jumping into different roles when needed to support the functioning of
+          my team, interacting with a variety of stakeholders and customer
+          personalities,gracefully<span> (and stealthily) </span>{' '}
+          troubleshooting problems, and learning how to do things faster, better
+          and cleaner each day to continually improve my craft. My time in
+          restaurants also enforced my passion for well thought out systems, and
+          the satisfaction that comes from the marriage of form, function and
+          aesthetics.
           <p />
           I transitioned into engineering because I knew I wanted to do
-          something bigger while still feeding this need to be creative,
-          collaborative, to fix and improve. Thus began the journey and I
-          enrolled into the Grace Hopper Academy,a full-time immersive software
-          engineering program centered on full stack JavaScript development.
-          As a recent graduate, I am currently open to all opportunities.
+          something bigger while still feeding my desire to build, fix, and
+          improve in a dynamic, creative, and collaborative setting. In the
+          summer of 2019, I enrolled in the The Grace Hopper program, an
+          intensive 17 week program centered on full stack JavaScript
+          development.
+          <p />
+          As a recent graduate, I am extremely motivated and open to any and all
+          opportunities.
           <p />
           If you are interested in working together, let's connect! You can
           reach me by choosing one of the options to your left.
